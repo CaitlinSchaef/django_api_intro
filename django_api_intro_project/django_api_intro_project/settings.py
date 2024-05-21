fa2ace4373266fb4f31268adf65daebc550fc1b4
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-21 14:59:54 -0400

    any updates

--- django_api_intro_project/django_api_intro_project/settings.py
+++ django_api_intro_project/django_api_intro_project/settings.py
@@ -48,6 +48,7 @@ GRAPH_MODELS = {
 }	
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +56,22 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:8080', 'http://localhost:5173/']
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
+
+CORS_ALLOW_HEADERS = [
+    'Content-Type',
+    'Authorization',
 ]
 
 ROOT_URLCONF = 'django_api_intro_project.urls'
